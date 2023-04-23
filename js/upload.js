26e5d7c963533ef0ad00cff812859d045453e81d
Thạch Tấn Lộc <Locsv142@gmail.com>
2023-04-23 23:51:56 +0700

    Nut Convert Da chay

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,8 @@
-const fileInput = document.getElementById('jsonFile');
+const fileInput  = document.getElementById('jsonFile');
+const convertBtn = document.getElementById('convert-btn');
+const outputdiv     = document.getElementById('output')
+
+
 fileInput.addEventListener('change', (event) => {
   const file = event.target.files[0];
   const reader = new FileReader();
@@ -6,11 +10,16 @@ fileInput.addEventListener('change', (event) => {
     const content = event.target.result;
     const jsonData = JSON.parse(content);
     const formattedData = JSON.stringify(jsonData, null, 2);
-    document.getElementById('output').innerText = formattedData;
+    
+    outputdiv.innerText = formattedData;
   };
   reader.readAsText(file);
 });
 
 
+convertBtn.addEventListener('click', () => {
+  const jsonData = JSON.parse(document.getElementById('output').innerText);
+  outputdiv.innerHTML= "<h2>Chay Nut Covert </h2> "
+});
 
   
